5bbf5331c820513355f453e405de0770e40ffb6d
p-lacerda <xcloudgameemail@gmail.com>
2022-04-05 21:28:21 -0300

    Implements list of posts in the main screen

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,18 +1,20 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 
-function PostCard() {
+function PostCard({
+  title, username, content, date,
+}) {
   return (
     <div className="post-card">
       <div>
-        <h4>Title</h4>
-        <p>Remover</p>
-        <p>Editar</p>
+        <h4>{ title }</h4>
+        <button type="button">Editar</button>
+        <button type="button">Remover</button>
       </div>
       <div>
-        <p>@Username</p>
-        <p>Content</p>
-        <p>X minutes ago</p>
+        <p>{username}</p>
+        <p>{ content }</p>
+        <p>{ String(date) }</p>
       </div>
     </div>
   );
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 import { connect } from 'react-redux';
 
@@ -5,21 +6,28 @@ import PostCard from './PostCard';
 
 function PostList(props) {
   const { allPosts } = props;
+  console.log(allPosts);
+
   return (
     <div className="post-list">
-      {/* There is not posts yet. Create the first! */}
-      { allPosts.map(() => {
+      There is not posts yet. Create the first!
+      { allPosts === undefined ? <p>Tem nada</p> : allPosts.map(({
+        title, username, content, date,
+      }) => (
         <PostCard
-        
+          title={title}
+          username={username}
+          content={content}
+          date={date}
         />
-      })}
+      ))}
     </div>
   );
 }
 
 const mapStateToProps = (state) => {
-  const { post: { allPosts } } = state;
-  return allPosts;
+  const { post } = state;
+  return post;
 };
 
 export default connect(mapStateToProps, null)(PostList);
